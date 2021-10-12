95e3bd9ffc8f1b9b057dd2ba4d26e450de5407b0
unknown <llopeza@alumno.unsm.edu.pe>
2021-10-12 00:54:32 -0500

    se creo la funcion de codificar

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,8 @@
 const cipher = {
-  // ...
+  encode:function (parametro1,parametro2){
+    console.log(parametro1);
+    console.log(parametro2);
+  }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+document.getElementById("text1").addEventListener("keyup", funcmayus);
+function funcmayus(){
+  let mayuscula=document.getElementById("text1");
+  mayuscula.value=mayuscula.value.toUpperCase();
+}
+document.getElementById('encode').addEventListener('click', encode);
+function encode() {
+    let var1=document.getElementById("text1").value;
+    let var2=document.getElementById("displacement").value;
+    cipher.encode(var1, var2);
+    }
+
+
