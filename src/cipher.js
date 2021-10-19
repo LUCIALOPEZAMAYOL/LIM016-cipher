86a2229a9f05f093109f6021c4083cc75b3635d0
unknown <llopeza@alumno.unsm.edu.pe>
2021-10-19 01:54:13 -0500

    funcion cifrar texto terminado

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,21 @@
+
 const cipher = {
-  encode:function (parametro1,parametro2){
-    console.log(parametro1);
-    console.log(parametro2);
+  encode:function (textCifrar,desplazar){
+    let TextoCifrado1="";
+    //console.log(textCifrar);
+    for (let i=0; i<textCifrar.length; i++){
+      let valorAscii=textCifrar.charCodeAt(i);
+      //console.log("valor: " + valorAscii);
+      if(valorAscii>=65 && valorAscii<=90){
+        let desplazamiento= (((valorAscii-65 + parseInt(desplazar))%26)+65);
+        //console.log("desplazamiento " + desplazamiento);
+        TextoCifrado1+=String.fromCharCode(desplazamiento);
+        //console.log("texto cifrado 1: " + TextoCifrado1);
+      };
+    };
+    return TextoCifrado1; 
   }
 };
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@ function funcmayus(){
 }
 document.getElementById('encode').addEventListener('click', encode);
 function encode() {
-    let var1=document.getElementById("text1").value;
-    let var2=document.getElementById("displacement").value;
-    cipher.encode(var1, var2);
+    let string=document.getElementById("text1").value;
+    //console.log(string);
+    let offset=document.getElementById("displacement").value;
+    let TextoCifrado2=cipher.encode(string,offset);
+    //console.log(TextoCifrado2);
+    document.getElementById("text1").value= TextoCifrado2; 
     }
-
-
